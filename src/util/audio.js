3c91d13cf57568712d9fdd7e0470db3e28f8398e
raman <ramanjit456@yahoo.co.in>
2015-11-14 20:52:04 -0500

    changed code to actually set pitch instead of just logged. Fixed z axis to zero and changed map value to include fortmisso number

--- src/util/audio.js
+++ src/util/audio.js
@@ -9,7 +9,7 @@ const mainVolume = ctx.createGain();
 // Connect the main volume node to the context destination.
 mainVolume.connect(ctx.destination);
 var fortissimo = ['ff','mf', 'pp'];
-export function play(pitch, xPercentage, yPercentage, zPercentage ) {
+export function play(pitch, xPercentage, yPercentage ) {
   // Create an object with a sound source and a volume control.
   const sound = {
     source: ctx.createBufferSource(),
@@ -23,32 +23,33 @@ export function play(pitch, xPercentage, yPercentage, zPercentage ) {
   // And hook up the panner to the main volume.
   sound.panner.connect(mainVolume);
   // Setup the audio position of the sound
-  sound.panner.setPosition(xPercentage / 100, yPercentage / 100, zPercentage / 100);
+  //set z index to zero for 2d sound
+  sound.panner.setPosition(xPercentage / 100, yPercentage / 100, 0);
   console.log("x percentage: ", xPercentage / 100);
   console.log("y percentage: ", yPercentage / 100);
-  console.log("z percentage: ", zPercentage / 100);
-
+  console.log("pitch number: ", pitch);
+  let fortissimoNo = Math.round((Math.random()*2));
   // Make the sound source loop.
   sound.source.loop = false;
   // Check if the cache has the current pitch in memory already
-  if (audioCache.has(pitch)) {
-    let buffer = audioCache.get(pitch);
+  if (audioCache.has(pitch + fortissimoNo)) {
+    let buffer = audioCache.get(pitch + fortissimoNo);
     sound.buffer = buffer;
     // Make the sound source use the buffer and start playing it
     sound.source.buffer = buffer;
     sound.source.start(ctx.currentTime);
   } else {
-    let request = new XMLHttpRequest();
-    request.open('GET', chrome.extension.getURL('/samples/mp3piano/Piano.ff.' + pitch + '.mp3'), true);
-    let fortissimoNo = Math.round((Math.random()*2));
-    console.log( fortissimoNo );
     console.log('/samples/mp3piano/Piano.' + fortissimo[fortissimoNo] + '.' + pitch + '.mp3');
+
+    let request = new XMLHttpRequest();
+    request.open('GET', chrome.extension.getURL('/samples/mp3piano/Piano.' + fortissimo[fortissimoNo] + '.' + pitch + '.mp3'), true);
+    
     request.responseType = 'arraybuffer';
     request.onload = function(e) {
       // Create a buffer from the response ArrayBuffer.
       ctx.decodeAudioData(this.response, function onSuccess(buffer) {
         // Store the sound in the audioCache
-        audioCache.set(pitch, buffer);
+        audioCache.set(pitch + fortissimoNo, buffer);
         sound.buffer = buffer;
         // Make the sound source use the buffer and start playing it
         sound.source.buffer = buffer;
